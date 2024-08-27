ef5466e02bbf2265b36a1bcc0a0f26106f77cfdf
Brett Mandler <brettmandler@gmail.com>
2024-08-27 15:38:05 -0400

    chore: Update package.json and tsconfig.json

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,9 @@
 import {defineConfig} from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/types.ts'],
   splitting: false,
+  dts: true,
   sourcemap: true,
   clean: true,
 })
